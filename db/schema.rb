c8aa5856201852cd71281f95356f94ec18dec3d6
Kaio Max <kaiomaks@gmail.com>
2015-09-26 10:51:02 -0300

    Generate Attraction resource and set its relation with User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150926134542) do
+ActiveRecord::Schema.define(version: 20150926134738) do
+
+  create_table "attractions", force: :cascade do |t|
+    t.string   "title"
+    t.string   "media"
+    t.datetime "schedule"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "attractions", ["user_id"], name: "index_attractions_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
